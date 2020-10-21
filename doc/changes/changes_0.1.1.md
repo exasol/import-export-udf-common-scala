a9fb5ae62a115c5a3a4fdeab3065184ee602a9b0
Muhammet Orazov <m.orazow@gmail.com>
2020-10-21 10:38:27 +0200

    #9: Added logging library as a common dependency (#10)
    Co-authored-by: Anastasiia Sergienko <46891819+AnastasiiaSergienko@users.noreply.github.com>

---
+++ doc/changes/changes_0.1.1.md
@@ -0,0 +1,20 @@
+# Import Export UDF Common Scala 0.1.1, released 2020-10-DD
+
+## Features
+
+* #9: Added SLF4J Logging Library as Common Dependency (PR #10)
+
+## Dependency Updates
+
+### Runtime Dependency Updates
+
+* Updated ``sbt.version`` from `1.3.13` to `1.4.1`.
+* Updated ``com.fasterxml.jackson.core:jackson-databind`` from `2.11.2` to `2.11.3`.
+
+### Test Dependency Updates
+
+* Updated ``org.mockito:mockito-core`` from `3.5.10` to `3.5.15`.
+
+### Plugin Updates
+
+* Updated ``com.github.cb372:sbt-explicit-dependencies`` from `0.2.13` to `0.2.14`.
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,13 +8,14 @@ object Dependencies {
   // Runtime dependencies versions
   private val ExasolVersion = "6.1.7"
   private val AvroVersion = "1.10.0"
-  private val JacksonVersion = "2.11.2"
+  private val JacksonVersion = "2.11.3"
   private val TypesafeLoggingVersion = "3.9.2"
+  private val SLF4JVersion = "1.7.30"
 
   // Test dependencies versions
   private val ScalaTestVersion = "3.2.2"
   private val ScalaTestPlusVersion = "1.0.0-M2"
-  private val MockitoCoreVersion = "3.5.10"
+  private val MockitoCoreVersion = "3.5.15"
 
   val ExasolResolvers: Seq[Resolver] = Seq(
     "Exasol Releases" at "https://maven.exasol.com/artifactory/exasol-releases"
@@ -22,6 +23,7 @@ object Dependencies {
 
   lazy val RuntimeDependencies: Seq[ModuleID] = Seq(
     "com.exasol" % "exasol-script-api" % ExasolVersion,
+    "org.slf4j" % "slf4j-simple" % SLF4JVersion,
     "com.typesafe.scala-logging" %% "scala-logging" % TypesafeLoggingVersion
       exclude ("org.slf4j", "slf4j-api")
       exclude ("org.scala-lang", "scala-library")
