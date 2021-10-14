074c425af8d87d4c134cdaa7bc6cf6cdb78f16c0
Muhammet Orazov <m.orazow@gmail.com>
2021-10-14 14:24:55 +0200

    #24: Fixed JSON mapper bug. (#25)
    Fixes #24

---
+++ doc/changes/changes_0.3.1.md
@@ -0,0 +1,20 @@
+# Import Export UDF Common Scala 0.3.1, released 2021-10-14
+
+Code name: Fixed JSON Mapper
+
+## Summary
+
+This release fixes parsing bugs in JSON mapper functionality.
+
+## Bug Fixes
+
+* #24: Fixed JSON mapper issues
+
+## Dependency Updates
+
+### Runtime Dependency Updates
+
+### Test Dependency Updates
+
+### Plugin Updates
+
--- src/main/scala/com/exasol/common/json/JsonMapper.scala
+++ src/main/scala/com/exasol/common/json/JsonMapper.scala
@@ -1,20 +1,67 @@
 package com.exasol.common.json
 
-import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.json.{JsonMapper => BaseJsonMapper}
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.ClassTagExtensions
 
+/**
+ * JSON parsing helper object class.
+ */
 object JsonMapper {
 
-  private[this] val mapper = BaseJsonMapper
-    .builder()
-    .addModule(DefaultScalaModule)
-    .build()
+  private[this] class ScalaJsonMapper(jsonMapper: BaseJsonMapper)
+      extends BaseJsonMapper(jsonMapper)
+      with ClassTagExtensions
 
+  private[this] val mapper = {
+    val builder = BaseJsonMapper
+      .builder()
+      .findAndAddModules()
+      .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+      .enable(JsonParser.Feature.ALLOW_COMMENTS)
+      .defaultMergeable(true)
+    new ScalaJsonMapper(builder.build())
+  }
+
+  /**
+   * Parses given value type into a JSON string.
+   *
+   * For parsing into pretty indented JSON use {@link toPrettyJson}.
+   *
+   * @param value a provided value type
+   * @return JSON string
+   */
   def toJson[T](value: T): String =
     mapper.writeValueAsString(value)
 
+  /**
+   * Parses given value into pretty JSON format.
+   *
+   * @param value a provided value type
+   * @return pretty JSON string
+   */
+  def toPrettyJson[T](value: T): String =
+    mapper.writer(SerializationFeature.INDENT_OUTPUT).writeValueAsString(value)
+
+  /**
+   * Parses JSON string into a type.
+   *
+   * @param jsonString a JSON string
+   * @return parsed value
+   */
+  def fromJson[T: Manifest](jsonString: String): T =
+    mapper.readValue[T](jsonString)
+
+  /**
+   * Parses JSON string into a type.
+   *
+   * @param jsonString a JSON string
+   * @return parsed value
+   */
+  @deprecated("Use fromJson method instead.", "0.3.1")
   def parseJson[T: Manifest](jsonString: String): T =
-    mapper.readValue[T](jsonString, new TypeReference[T]() {})
+    fromJson(jsonString)
 
 }
